848acd0ced2db6af74a9bb1d088c9f5e480b9f92
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-06-30 17:14:54 -0400

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Converter from './components/Converter';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <h1>Currency Converter</h1>
       </header>
       <Converter/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,5 @@
+export default function Footer(){
+    return (
+        <p id="githubLink"><a href="https://github.com/AndrewWilborn/coffee-list">GitHub</a></p>
+    )
+}
